9144d589e1a7deb5066d90cd0b4fdb770d614b2d
Barkın ZORLU <barkinzorlu@outlook.com>
2026-02-09 00:11:07 +0300

    Update formatter.cpp

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -10,19 +10,50 @@ std::vector<Formatter> parse_formatters(const std::string& line) {
         pos = line.find("**", pos);
         if (pos == std::string::npos) break;
         
-        size_t close_pos = line.find("**", pos + 2);
-        if (close_pos != std::string::npos) {
-            formatters.emplace_back(
-                Formatter::Type::BOLD,
-                pos,                    // start_index
-                close_pos + 2,         // end_index (after closing **)
-                pos + 2,               // content_start
-                close_pos,             // content_end
-                "**",
-                "**"
-            );
-            pos = close_pos + 2;
-        } else {
+        // Make sure ** is not part of *** or more stars
+        // Check if preceded by * (making it ***)
+        if (pos > 0 && line[pos - 1] == '*') {
+            pos++;
+            continue;
+        }
+        // Check if followed by * (making it ***)
+        if (pos + 2 < line.length() && line[pos + 2] == '*') {
+            pos++;
+            continue;
+        }
+        
+        size_t close_pos = pos + 2;
+        while (close_pos < line.length()) {
+            close_pos = line.find("**", close_pos);
+            if (close_pos == std::string::npos) break;
+            
+            // Make sure closing ** is not part of *** or more
+            bool valid_closing = true;
+            if (close_pos > 0 && line[close_pos - 1] == '*') {
+                valid_closing = false;
+            }
+            if (close_pos + 2 < line.length() && line[close_pos + 2] == '*') {
+                valid_closing = false;
+            }
+            
+            if (valid_closing) {
+                formatters.emplace_back(
+                    Formatter::Type::BOLD,
+                    pos,                    // start_index
+                    close_pos + 2,         // end_index (after closing **)
+                    pos + 2,               // content_start
+                    close_pos,             // content_end
+                    "**",
+                    "**"
+                );
+                pos = close_pos + 2;
+                break;
+            } else {
+                close_pos++;
+            }
+        }
+        
+        if (close_pos == std::string::npos) {
             pos += 2;
         }
     }
@@ -56,19 +87,48 @@ std::vector<Formatter> parse_formatters(const std::string& line) {
         pos = line.find("~~", pos);
         if (pos == std::string::npos) break;
         
-        size_t close_pos = line.find("~~", pos + 2);
-        if (close_pos != std::string::npos) {
-            formatters.emplace_back(
-                Formatter::Type::STRIKETHROUGH,
-                pos,                    // start_index
-                close_pos + 2,         // end_index (after closing ~~)
-                pos + 2,               // content_start
-                close_pos,             // content_end
-                "~~",
-                "~~"
-            );
-            pos = close_pos + 2;
-        } else {
+        // Make sure ~~ is not part of ~~~ or more tildes
+        if (pos > 0 && line[pos - 1] == '~') {
+            pos++;
+            continue;
+        }
+        if (pos + 2 < line.length() && line[pos + 2] == '~') {
+            pos++;
+            continue;
+        }
+        
+        size_t close_pos = pos + 2;
+        while (close_pos < line.length()) {
+            close_pos = line.find("~~", close_pos);
+            if (close_pos == std::string::npos) break;
+            
+            // Make sure closing ~~ is not part of ~~~ or more
+            bool valid_closing = true;
+            if (close_pos > 0 && line[close_pos - 1] == '~') {
+                valid_closing = false;
+            }
+            if (close_pos + 2 < line.length() && line[close_pos + 2] == '~') {
+                valid_closing = false;
+            }
+            
+            if (valid_closing) {
+                formatters.emplace_back(
+                    Formatter::Type::STRIKETHROUGH,
+                    pos,                    // start_index
+                    close_pos + 2,         // end_index (after closing ~~)
+                    pos + 2,               // content_start
+                    close_pos,             // content_end
+                    "~~",
+                    "~~"
+                );
+                pos = close_pos + 2;
+                break;
+            } else {
+                close_pos++;
+            }
+        }
+        
+        if (close_pos == std::string::npos) {
             pos += 2;
         }
     }
@@ -79,7 +139,7 @@ std::vector<Formatter> parse_formatters(const std::string& line) {
         pos = line.find('*', pos);
         if (pos == std::string::npos) break;
         
-        // Skip if it's part of **
+        // Skip if it's part of ** or ***
         if (pos + 1 < line.length() && line[pos + 1] == '*') {
             pos += 2;
             continue;
@@ -96,27 +156,30 @@ std::vector<Formatter> parse_formatters(const std::string& line) {
             if (close_pos == std::string::npos) break;
             
             // Make sure it's not part of **
+            bool valid_closing = true;
             if (close_pos + 1 < line.length() && line[close_pos + 1] == '*') {
-                close_pos += 2;
-                continue;
+                valid_closing = false;
             }
             if (close_pos > 0 && line[close_pos - 1] == '*') {
-                close_pos++;
-                continue;
+                valid_closing = false;
             }
             
-            // Found valid closing *
-            formatters.emplace_back(
-                Formatter::Type::ITALIC,
-                pos,                    // start_index
-                close_pos + 1,         // end_index (after closing *)
-                pos + 1,               // content_start
-                close_pos,             // content_end
-                "*",
-                "*"
-            );
-            pos = close_pos + 1;
-            break;
+            if (valid_closing) {
+                // Found valid closing *
+                formatters.emplace_back(
+                    Formatter::Type::ITALIC,
+                    pos,                    // start_index
+                    close_pos + 1,         // end_index (after closing *)
+                    pos + 1,               // content_start
+                    close_pos,             // content_end
+                    "*",
+                    "*"
+                );
+                pos = close_pos + 1;
+                break;
+            } else {
+                close_pos++;
+            }
         }
         
         if (close_pos == std::string::npos) {
